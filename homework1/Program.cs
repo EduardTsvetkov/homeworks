2302522414f541a6516c2b07d7245c1a4ff00962
EduardTsvetkov <edvardas@inbox.ru>
2022-08-07 17:48:39 +0300

    Добавлены еще пара задач

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -10,3 +10,36 @@ if (a > max)
 }
 Console.WriteLine("Максимальное число: " + max);
 Console.WriteLine();
+
+//-----
+
+Console.WriteLine();
+Console.WriteLine("2. Определяем максимальное из трех чисел.");
+Console.Write("Введите первое число: ");
+max = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите второе число: ");
+a = Convert.ToInt16(Console.ReadLine());
+if (a > max) max = a;
+Console.Write("Введите третье число: ");
+a = Convert.ToInt16(Console.ReadLine());
+if (a > max) max = a;
+Console.WriteLine("Максимальное число: " + max);
+Console.WriteLine();
+
+//-----
+
+Console.WriteLine();
+Console.WriteLine("3. Определяем четное или нет.");
+Console.Write("Введите число: ");
+a = Convert.ToInt16(Console.ReadLine());
+if (a % 2 == 0) 
+{
+    Console.Write("Число " + a + " четное");
+}
+else
+{
+    Console.Write("Число " + a + " нечетное");
+}
+
+//-----
+
