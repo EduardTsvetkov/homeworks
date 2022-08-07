7cbc8a7de7628ce5f6bf01002d9411f047b4bf09
EduardTsvetkov <edvardas@inbox.ru>
2022-08-07 17:27:11 +0300

    Добавлена первая задача

---
+++ homework1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine();
+Console.WriteLine("1. Определяем максимальное из двух чисел.");
+Console.Write("Введите первое число: ");
+int max = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int a = Convert.ToInt16(Console.ReadLine());
+if (a > max)
+{
+    max = a;
+}
+Console.WriteLine("Максимальное число: " + max);
+Console.WriteLine();
