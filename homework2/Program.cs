c600423db11d14a49fc2d37b7c3d464fd174ba34
EduardTsvetkov <edvardas@inbox.ru>
2022-08-12 00:06:18 +0300

    Решена задача 10

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+bool makeСhoice(string question)
+{
+    Console.WriteLine(question);
+    while (true)
+    {
+        Console.Write("Ведите Y (да) или N (нет): ");  
+        string choice = Console.ReadLine();
+        choice = choice.ToLower();
+        if (choice == "y")
+        {
+            return true;
+        }      
+        if (choice == "n")
+        {
+            return false;
+        }
+    }
+    
+}
+
+int getThreeDigit() 
+{
+    while (true)
+    {
+        Console.Write("Ведите трехзначное число: ");  
+        int num3 = Convert.ToInt16(Console.ReadLine());
+        if (num3 > 99 & num3 < 1000)
+        {
+            return num3;
+        }  
+        Console.WriteLine($"Число {num3} не трехзначное!");
+    }    
+}
+
+int getMiddleNumber(int num)
+{
+    return (num /10 % 10);
+}
+
+
+
+while (makeСhoice("Решаем задачу 10?"))
+{
+    int number3 = getThreeDigit();
+    Console.Write($"В числе {number3} средняя цифра ");
+    Console.WriteLine(getMiddleNumber(number3));
+    Console.WriteLine();
+
+}
+
