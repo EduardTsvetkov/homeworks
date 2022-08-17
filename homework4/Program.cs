a79642adf2e58f8f66fe5c0bacdbbefea4edb795
EduardTsvetkov <edvardas@inbox.ru>
2022-08-18 00:28:32 +0300

    Задача 25 выполнена

--- homework4/Program.cs
+++ homework4/Program.cs
@@ -10,3 +10,75 @@
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+bool makeСhoice(string question)  // определяем выполнять (повторять) задачу или переходить к следующей
+{
+    Console.WriteLine(question);
+    while (true)
+    {
+        Console.Write("Ведите Y (да) или N (нет): ");
+        string choice = Console.ReadLine();
+        choice = choice.ToLower();
+        if (choice == "y")
+        {
+            return true;
+        }
+        if (choice == "n")
+        {
+            Console.WriteLine();
+            return false;
+        }
+    }
+
+}
+
+
+
+int GetInt()  // получаем целое число
+{
+    string num = Console.ReadLine();
+    while (true)
+    {
+        bool isInt = int.TryParse(num, out int resultInt);
+        if (isInt)
+        {
+            return resultInt;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не целое число.");
+            Console.Write("Введите целое число: ");
+            num = Console.ReadLine();
+        }
+    }
+}
+
+int RaiseNumberPower(int baseNum, int indexNum)  // пишем свой метод pow  :-)
+{
+    int result = baseNum;
+    for (int i = 1; i < indexNum; i++)
+    {
+        result *= baseNum;
+    }
+    return result;
+}
+
+
+
+
+
+while (makeСhoice("Решаем задачу 25 (возведение в степень)?"))
+{
+    Console.Write("Введите целое число A: ");
+    int varA = GetInt();
+    Console.Write("Введите натуральное число B (больше 0): ");
+    int varB = GetInt();
+    while (varB <= 0)
+    {
+        Console.WriteLine($"{varB} меньше или равно 0: ");
+        Console.Write("Введите натуральное число B (больше 0): ");
+        varB = GetInt();
+    }
+    Console.WriteLine($"{varA} в степени {varB} равно {RaiseNumberPower(varA, varB)}");
+    Console.WriteLine();
+}
