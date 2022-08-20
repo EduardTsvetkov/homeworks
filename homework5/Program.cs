acc6a63b08716e46017d7bc655e91a13a4ddef3c
EduardTsvetkov <edvardas@inbox.ru>
2022-08-20 15:52:31 +0300

    initial commit omeork5

---
+++ homework5/Program.cs
@@ -0,0 +1,32 @@
+﻿bool MakeСhoice(string question)  // получаем ответ пользователя на вопрос
+{
+    Console.WriteLine(question);
+    while (true)
+    {
+        Console.Write("Ведите Y (да) или N (нет): ");
+        string choice = Console.ReadLine();
+        choice = choice.ToLower();
+        if (choice == "y")
+        {
+            return true;
+        }
+        if (choice == "n")
+        {
+            Console.WriteLine();
+            return false;
+        }
+    }
+}
+
+//------------
+
+void PrintIntArray(int[] inputArray)  // вывод на экран массива целых чисел
+{
+    foreach (int item in inputArray) 
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+//------------
+
