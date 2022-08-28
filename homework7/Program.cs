70fba1ddd7960b9b1609f5ab4ca9c227a5489962
EduardTsvetkov <edvardas@inbox.ru>
2022-08-29 00:21:39 +0300

    Задача 50 выполнена

--- homework7/Program.cs
+++ homework7/Program.cs
@@ -22,7 +22,7 @@
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
 
-bool MakeСhoice(string question)  // получаем ответ пользователя на вопрос
+bool MakeChoice(string question)  // получаем ответ пользователя на вопрос
 {
     Console.WriteLine(question);
     while (true)
@@ -44,6 +44,23 @@ bool MakeСhoice(string question)  // получаем ответ пользов
 
 //------------
 
+int GetIntInResponce(string request)
+{
+    string answer = String.Empty;
+    while (true)
+    {
+        Console.Write(request);
+        answer = Console.ReadLine();
+        if (int.TryParse(answer, out int result))
+        {
+            return result;
+        }
+        Console.WriteLine("Вы ввели не целое число. ");
+    }
+}
+
+//------------
+
 double[,] FillDoubleArray2D(int m, int n, int accuracyCalculations)  // получаем размер массива и точность вычисления
 {
     double[,] outputArray = new double[m, n];
@@ -52,7 +69,23 @@ double[,] FillDoubleArray2D(int m, int n, int accuracyCalculations)  // полу
     {
         for (int j = 0; j < n; j++)
         {
-            outputArray[i, j] = Math.Round(random.Next(-10, 11) * random.NextDouble(), accuracyCalculations);
+            outputArray[i, j] = Math.Round(random.Next(-100, 101) * random.NextDouble(), accuracyCalculations);
+        }    
+    }
+    return outputArray;
+}
+
+//------------
+
+int[,] FillIntArray2D(int m, int n, int min, int max)  // получаем размер массива и точность вычисления
+{
+    int[,] outputArray = new int[m, n];
+    Random random = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            outputArray[i, j] = random.Next(min, max + 1);
         }    
     }
     return outputArray;
@@ -62,46 +95,99 @@ double[,] FillDoubleArray2D(int m, int n, int accuracyCalculations)  // полу
 
 void PrintDoubleArray2D(double[,] inputArray)
 {
+    string max = Convert.ToString(inputArray.Cast<double>().Max());  // находит максимальный элемент массива и в стринг
+    string min = Convert.ToString(inputArray.Cast<double>().Min());  // находит минимальный элемент массива и в стринг
+    int len = Math.Max(max.Length, min.Length) + 1;                  // определяем размер поля (не совсем корректно...)
+    string forFormat = "{0, " + Convert.ToString(len) + "}";
     for (int i = 0; i < inputArray.GetLength(0); i++)
     {
         for (int j = 0; j < inputArray.GetLength(1); j++)
         {
-//            Console.Write($"{inputArray[i,j]} ");
-              Console.Write(String.Format("{0, 6}", inputArray[i, j]));
+              Console.Write(String.Format(forFormat, inputArray[i, j]));
         }
         Console.WriteLine();
     }
 }
 
+//------------
+
+void PrintIntArray2D(int[,] inputArray)  // вывод на экран массива целых чисел
+{
+    string max = Convert.ToString(inputArray.Cast<int>().Max());  // находит максимальный элемент массива и в стринг
+    string min = Convert.ToString(inputArray.Cast<int>().Min());  // находит минимальный элемент массива и в стринг
+    int len = Math.Max(max.Length, min.Length) + 1;               // определяем размер поля для печати 
+    string forFormat = "{0, " + Convert.ToString(len) + "}";      // формируем строку типа "{0, 4}"
+
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+              Console.Write(String.Format(forFormat, inputArray[i, j]));
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+bool FindingNumber(int[,] inputArray, int number, out int indexI, out int indexJ)
+{
+    indexI = -1;
+    indexJ = -1;
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            if (inputArray[i, j] == number)
+            {
+                indexI = i;
+                indexJ = j;
+                return true;
+            }
+        }
+        
+    }   
+    return false; 
+}
 
 //------------ Задачи
 
 Console.Clear();
-while (MakeСhoice("Выполняем задачу 47 (заполнение двумерного массива выещественными числами)?"))
+while (MakeChoice("Выполняем задачу 47 (заполнение двумерного массива выещественными числами)?"))
 {
-    bool isInt;
-    int rows, columns, accuracy;
-    do
-    {
-        Console.Write("Введите количество строк: ");
-        isInt = int.TryParse(Console.ReadLine(), out rows);
-    } while (!isInt);
 
-    do
-    {
-        Console.Write("Введите количество столбцов: ");
-        isInt = int.TryParse(Console.ReadLine(), out columns);
-    } while (!isInt);
-
-    do
-    {
-        Console.Write("Введите точность (количество знаков после запятой): ");
-        isInt = int.TryParse(Console.ReadLine(), out accuracy);
-    } while (!isInt);
+    int rows1 = GetIntInResponce("Введите количество строк: ");
+    int columns1 = GetIntInResponce("Введите количество столбцов: ");
+    int accuracy = GetIntInResponce("Введите точность (количество знаков после запятой): ");
+    
     Console.WriteLine();
 
-    double[,] array = FillDoubleArray2D(rows, columns, accuracy);
-    PrintDoubleArray2D(array);
+    double[,] array1 = FillDoubleArray2D(rows1, columns1, accuracy);
+    PrintDoubleArray2D(array1);
     Console.WriteLine();
 }
 
+
+while (MakeChoice("Решаем задачу 50 (поиск элемента массива)?"))
+{
+    int rows2 = GetIntInResponce("Введите количество строк: ");
+    int columns2 = GetIntInResponce("Введите количество столбцов: ");    
+    int numFrom = GetIntInResponce("Массив заполняем числами от: ");
+    int numTo = GetIntInResponce("Массив заполняем числами до: ");
+    
+    int[,] array2 = FillIntArray2D(rows2, columns2, numFrom, numTo);
+
+    PrintIntArray2D(array2);
+
+    int findNumber = GetIntInResponce("Введите искомое число: ");
+    int indI, indJ;
+    if (FindingNumber(array2, findNumber, out indI, out indJ))
+    {
+        Console.WriteLine($"У числа {findNumber} в массиве индекс [{indI}, {indJ}]. ");
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.WriteLine($"Числа {findNumber} нет в массиве. ");
+        Console.WriteLine();
+    }
+}
