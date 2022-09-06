913fa0b40b9c21e2ddd534c5915365a77542fec5
EduardTsvetkov <edvardas@inbox.ru>
2022-09-06 20:04:24 +0300

    Спираль готова

--- Tests/test1.cs
+++ Tests/test1.cs
@@ -4,7 +4,7 @@
     string min = Convert.ToString(inputArray.Cast<int>().Min());  // находит минимальный элемент массива и в стринг
     int len = Math.Max(max.Length, min.Length) + 1;               // определяем размер поля для печати 
     string forFormat = "{0, " + Convert.ToString(len) + "}";      // формируем строку типа "{0, 4}"
-
+ 
     for (int i = 0; i < inputArray.GetLength(0); i++)
     {
         for (int j = 0; j < inputArray.GetLength(1); j++)
@@ -14,59 +14,46 @@
         Console.WriteLine();
     }
 }
-
-int rows = 3, columns = 3;
-int initX = 0, initY = 0;
-int initNum = 0;
-int[,] array = new int[rows,columns];
-FillSpiral(rows, columns, array, initX, initY, initNum);
-
-initNum = 2*rows + 2*columns - 4;
-initX++; initY++;
-rows -= 2;
-columns -=2;
-FillSpiral(rows, columns, array, initX, initY, initNum);
-
-
-
-void FillSpiral(int Y, int X, int[,] arr, int col, int row, int num){
-
-int dX = 0, dY = 0;
-
-for (int i = 1; i < X + Y + X + Y - 3; i++)
+ 
+int rows = 5, columns = 6;
+int[,] spiralArr = FillSpiral(rows, columns);
+ 
+int[,] FillSpiral(int Y, int X)
 {
-    arr[row,col] = i + num;
-    if ((X - i) > 0)  // идем по верху
+    int[,] arr = new int[Y, X];
+    int col = 0;
+    int row = 0;
+    int dX = 0, dY = 0;
+    int num = 0;
+    for (int i = 1; i < arr.Length + 1; i++)
     {
-        dX = 1; dY = 0;
-    }
-    else if ((X + Y - 1 - i) > 0) // идем по правому краю
-    {
-        dX = 0; dY = 1;
-    }
-    else if ((2 * X + Y - 2 - i) > 0)
-    {
-        dX = -1; dY = 0;
-    }
-    else if ((2 * X + 2 * Y - 3 - i) > 0)
-    {
-        dX = 0; dY = -1;
-    }
-    else
-    {
-        dX = 1; dY = 0;
-
-    }
-
-
-
+        arr[row, col] = i;
+        if ((X - i) + num > 0)  // идем по верху
+        {
+            dX = 1; dY = 0;
+        }
+        else if ((X + Y - 1 - i) + num > 0) // идем по правому краю
+        {
+            dX = 0; dY = 1;
+        }
+        else if ((2 * X + Y - 2 - i) + num > 0)  // идем по низу
+        {
+            dX = -1; dY = 0;
+        }
+        else if ((2 * X + 2 * Y - 4 - i) + num > 0)  // идем по правому краю
+        {
+            dX = 0; dY = -1;
+        }
+        else                                         // вносим корректировки (уменьшаем размер спирали)
+        {
+            dX = 1; dY = 0;
+            X -= 2; Y -= 2;
+            num = i;
+        }
         row += dY;
         col += dX;
-    
-
+    }
+    return arr;
 }
-}
-
-
-
-PrintIntArray2D(array);
+ 
+PrintIntArray2D(spiralArr);
